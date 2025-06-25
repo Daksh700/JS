e94266c8f7b1c94ee43a821112820fc00147cab4
Daksh Goel <138396914+Daksh700@users.noreply.github.com>
2025-06-25 09:25:50 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,34 @@
+let score = undefined
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber));
+
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// null => 0
+
+let isLoggedIn = "daksh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "" => false
+// "daksh" => true
+
+let someNumber = 44
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+
+// 44 => 44 -> typeof -> string
+
