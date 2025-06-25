dc3bc5c16380307153b3214d3e9f71795475d4cc
Daksh Goel <138396914+Daksh700@users.noreply.github.com>
2025-06-25 09:43:12 +0000

    comparsion operations2

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,12 +1,12 @@
 let score = undefined
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 let valueInNumber = Number(score);
 
-console.log(typeof(valueInNumber));
+// console.log(typeof(valueInNumber));
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -18,7 +18,7 @@ let isLoggedIn = "daksh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true
 // "" => false
@@ -28,7 +28,34 @@ let someNumber = 44
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
+// console.log(stringNumber);
 
 // 44 => 44 -> typeof -> string
 
+
+let value = 3
+
+let negativeValue = -value;
+
+console.log(negativeValue);
+
+let str1 = "hello"
+
+let str2 = " daksh"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 101
+
+console.log(gameCounter++);
