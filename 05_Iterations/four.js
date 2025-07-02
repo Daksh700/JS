3b86ef0f6097328f3260a434fed32a739a2d5c30
Daksh Goel <138396914+Daksh700@users.noreply.github.com>
2025-07-02 12:13:53 +0000

    for-in loop

---
+++ 05_Iterations/four.js
@@ -0,0 +1,27 @@
+const myObj = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    py: 'python'
+}
+
+// for (const key in myObj) {
+//     console.log(`${key} shortcut is for ${myObj[key]}`);
+// }
+
+const programming = ["JS", "Ruby", "Python", "CPP"]
+
+// for (const key in programming) { // keys 
+//     console.log(programming[key]);
+// }
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
